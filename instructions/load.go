f484f59702de027f438504f44ae601627e1b1eb1
tinycedar <qienlin@outlook.com>
2017-06-14 00:12:16 +0800

    misc: refactor load instruction

--- instructions/load.go
+++ instructions/load.go
@@ -10,8 +10,7 @@ type iload_1 struct {
 }
 
 func (i *iload_1) Execute(f *runtime.Frame) {
-	val := f.LocalVars().GetInt(1)
-	f.OperandStack().PushInt(val)
+	_iload(f, 1)
 }
 
 func (i *iload_1) String() string {
@@ -23,10 +22,14 @@ type iload_2 struct {
 }
 
 func (i *iload_2) Execute(f *runtime.Frame) {
-	val := f.LocalVars().GetInt(2)
-	f.OperandStack().PushInt(val)
+	_iload(f, 2)
 }
 
 func (i *iload_2) String() string {
 	return fmt.Sprintf("{opcode: 0x%x, iload_2}", i.opCode)
+}
+
+func _iload(f *runtime.Frame, index int) {
+	val := f.LocalVars().GetInt(index)
+	f.OperandStack().PushInt(val)
 }
