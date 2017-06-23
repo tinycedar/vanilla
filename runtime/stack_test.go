ef314ab15670dfb5cb82764e138d1b52ff379f9b
tinycedar <qienlin@outlook.com>
2017-06-24 00:45:59 +0800

    misc: fix test failure

--- runtime/stack_test.go
+++ runtime/stack_test.go
@@ -7,9 +7,9 @@ import (
 
 func TestStack_Push(t *testing.T) {
 	stack := newStack()
-	stack.Push(NewFrame(1, 11))
-	stack.Push(NewFrame(2, 22))
-	stack.Push(NewFrame(3, 33))
+	stack.Push(NewFrame(nil, 0, 0, nil))
+	stack.Push(NewFrame(nil, 1, 11, nil))
+	stack.Push(NewFrame(nil, 2, 22, nil))
 	fmt.Println(stack)
 	stack.Pop()
 	fmt.Println(stack)
